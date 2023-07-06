ad5ef282ab78dac4f47e9e56129ab37c8b6acbbb
jtcfred <jtcfred@gmail.com>
2023-07-06 14:01:30 -0400

    Added quiz data

--- src/app/SomethinSomethin/page.js
+++ src/app/SomethinSomethin/page.js
@@ -1,20 +1,33 @@
 'use client'
-import Image from 'next/image'
-import { Box, Heading, Center, Flex } from '@chakra-ui/react'
-import mushroom from '/public/vercel.svg'
 
-export default function BirthdayWishes() {
+import { Box, Heading, Stack, Flex, Container, Text, Button } from '@chakra-ui/react'
+
+
+export default function SpotifyPage() {
   return (
     <main>
-        <Flex alignItems="center" justifyContent="space-around">
-
-          <Box borderRadius="20px" border="4px" borderColor="jordan.100" w="max-content" p="20px 80px" mt="20">
-            <Heading as="h1" size="4xl" textAlign="center" color="jordan.100">
-              Happy Birthday Jordan!
-            </Heading>
-          </Box>
-
-        </Flex>
+      <Container maxW={'3xl'} minH={"70vh"}>
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 33 }}>
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '4xl', sm: '5xl', md: '6xl', lg: '100px' }}
+            lineHeight={'110%'}
+            color="jordan.300">
+            Do a happy<br />
+            <Text as={'span'} color={'jordan.100'}>
+              Birthday Dance
+            </Text>
+          </Heading>
+          <Text color={'white'} fontSize="xl">
+            If I am a good programmer, there will be a spotify playlist embedded below this text. Listen through Jordan&apos;s Birthday playlist
+            and add songs when it&apos;s open to collaboration!
+          </Text>
+        </Stack>
+      </Container>
     </main>
   )
 }
--- src/components/QuizCTA.js
+++ src/components/QuizCTA.js
@@ -9,47 +9,45 @@ import {
 
 export default function QuizCTA({ handleClick }) {
     return (
-        <>
-            <Container maxW={'3xl'} minH={"70vh"}>
-                <Stack
-                    as={Box}
-                    textAlign={'center'}
-                    spacing={{ base: 8, md: 14 }}
-                    py={{ base: 20, md: 33}}>
-                    <Heading
-                        fontWeight={600}
-                        fontSize={{ base: '4xl', sm: '5xl', md: '6xl', lg: '100px' }}
-                        lineHeight={'110%'}
-                        color="jordan.300">
-                        It&apos;s time for<br/>
-                        <Text as={'span'} color={'jordan.100'}>
-                            Jordan&apos;s Birthday Quiz
-                        </Text>
-                    </Heading>
-                    <Text color={'white'} fontSize="xl">
-                        I have written an action packed Jordan birthday quiz
-                        to put to the test your working knowledge of Jordan Stortz.
-                        She kinda has a lot going on so don&apos;t be suprised if there
-                        is stuff you don&apos;t know.
+        <Container maxW={'3xl'} minH={"70vh"}>
+            <Stack
+                as={Box}
+                textAlign={'center'}
+                spacing={{ base: 8, md: 14 }}
+                py={{ base: 20, md: 33 }}>
+                <Heading
+                    fontWeight={600}
+                    fontSize={{ base: '4xl', sm: '5xl', md: '6xl', lg: '100px' }}
+                    lineHeight={'110%'}
+                    color="jordan.300">
+                    It&apos;s time for<br />
+                    <Text as={'span'} color={'jordan.100'}>
+                        Jordan&apos;s Birthday Quiz
                     </Text>
-                    <Button
-                        colorScheme={'jordan.100'}
-                        fontSize={{ base: '2xl', sm: '4xl', md: '6xl', lg: '100px' }}
-                        p={{base: "lg", lg: "50px"}}
-                        bg={'jordan.100'}
-                        color="jordan.200"
-                        rounded={'full'}
-                        px={6}
-                        _hover={{
-                            bg: 'jordan.300',
-                            transform: "scale(1.1)"
-                        }}
-                        onClick={handleClick}
-                        >
-                        Get Started
-                    </Button>
-                </Stack>
-            </Container>
-        </>
+                </Heading>
+                <Text color={'white'} fontSize="xl">
+                    I have written an action packed Jordan birthday quiz
+                    to put to the test your working knowledge of Jordan Stortz.
+                    She kinda has a lot going on so don&apos;t be suprised if there
+                    is stuff you don&apos;t know.
+                </Text>
+                <Button
+                    colorScheme={'jordan.100'}
+                    fontSize={{ base: '2xl', sm: '4xl', md: '6xl', lg: '100px' }}
+                    p={{ base: "lg", lg: "50px" }}
+                    bg={'jordan.100'}
+                    color="jordan.200"
+                    rounded={'full'}
+                    px={6}
+                    _hover={{
+                        bg: 'jordan.300',
+                        transform: "scale(1.1)"
+                    }}
+                    onClick={handleClick}
+                >
+                    Get Started
+                </Button>
+            </Stack>
+        </Container>
     );
 }
